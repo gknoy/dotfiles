4a2237ce0f3b5d7691165aeaddf321167d17f038
Gabriel Knoy <gabriel.knoy@gmail.com>
2025-09-03 15:19:26 -0700

    Use xz instead of gzip

--- shell-imports/db-dump.sh
+++ shell-imports/db-dump.sh
@@ -3,15 +3,27 @@
 # dump-pg some-name
 function dump-pg () {
     # Write a named sql dump
+    # use xz instead of gzip
     docker exec -t $(docker ps | grep postgres | cut -c -12) \
            pg_dumpall -c -U postgres \
-        | gzip > ~/tmp/db-dumps/${1:-dump}_$(date +%d-%m-%Y"_"%H_%M_%S).sql.gz
+        | xz -z -e > ~/tmp/db-dumps/${1:-dump}_$(date +%d-%m-%Y"_"%H_%M_%S).sql.xz
 }
 
 # restore-pg filename
 function restore-pg () {
-    gunzip "${1}" --to-stdout | \
-        docker exec -i $(docker ps | grep postgres | cut -c -12) \
-               psql --quiet --echo-errors -U postgres \
-               | xargs -L1 echo -n '.'
+    fname="${1}"
+    ext=$fname:t:e
+    echo "fname: $fname\next: $ext"
+    if [[ $ext == "gz" ]]; then
+        gunzip "${1}" --to-stdout | \
+            docker exec -i $(docker ps | grep postgres | cut -c -12) \
+                psql --quiet --echo-errors -U postgres \
+                | xargs -L1 echo -n '.'
+    elif [[ $ext == "xz" ]]; then
+        xz --stdout "${1}" | \
+            docker exec -i $(docker ps | grep postgres | cut -c -12) \
+                psql --quiet --echo-errors -U postgres \
+                | xargs -L1 echo -n '.'
+    fi
+
 }
